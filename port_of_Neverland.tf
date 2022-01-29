ef44a7e55fb0718a4aa225c3a447607b7a124e59
root <root@cks-master2.europe-west6-b.c.academic-works-336914.internal>
2022-01-29 15:29:22 +0000

    ansible/terraform integration

--- port_of_Neverland.tf
+++ port_of_Neverland.tf
@@ -25,11 +25,14 @@ resource "google_compute_instance" "port-neverland" {
     access_config {
     }
   }
-metadata = {
-   ssh-keys = "${ var.ssh_user }:${file("./sensitive_data/gcloud.pub")}"
- }
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file("./sensitive_data/gcloud.pub")}"
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ${self.network_interface.0.access_config.0.nat_ip}, -u ${var.ssh_user} --private-key ./sensitive_data/gcloud.private playbook.yaml"
+  }
 }
 
 output "port-neverland-ip" {
- value = google_compute_instance.port-neverland.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.port-neverland.network_interface.0.access_config.0.nat_ip
 }
