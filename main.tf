2ca8713504b88768641d4d3cfffa18ceff4779df
mrneverman <mrneverman@users.noreply.github.com>
2022-01-31 20:51:24 +0000

    AN.0 phase of ansible and terraform done

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ output "nevertown-IP" {
 module "worktowns" {
   source                = "./terraform/modules/gci"
   instance_name         = each.key
-  instance_ansible_file = "playbook.yaml"
+  instance_ansible_file = "./ansible/worktown_init.yaml"
   ssh_user              = var.ssh_user
   for_each              = toset(var.worker_nodes_name)
   
@@ -25,20 +25,21 @@ output "worktowns-IPs" {
   }
 
 }
+
 module "port-of-neverland" {
   source                = "./terraform/modules/gci"
   instance_name         = "port-of-neverland"
-  instance_ansible_file = "playbook.yaml"
+  instance_ansible_file = "./ansible/port-of-neverland_init.yaml"
   ssh_user              = var.ssh_user
   
-  depends_on = [ module.nevertown,  module.worktowns ]
+  depends_on = [ module.nevertown]
 
 }
 
 output "port_of_neverland-IP" {
   value = module.port-of-neverland.ip
 }
-
+/*
 module "island-of-intelligence" {
   source                = "./terraform/modules/gci"
   instance_name         = "island-of-intelligence"
@@ -51,4 +52,4 @@ module "island-of-intelligence" {
 output "island-of-intelligence-IP" {
   value = module.island-of-intelligence.ip
 }
-
+*/
